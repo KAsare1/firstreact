83919ed4d23a8c34630e584055b4cf11ff835a3a
KAsare1 <asarek977@gmail.com>
2023-01-30 23:00:39 +0000

    Using State
    Learning State management

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useState } from 'react';
 
 export default function App() {
-  const [name, setName] = useState("Kwame");
+  const [name, setName] = useState("Kofi");
   return (
     <View style={styles.container}>
-      <Text>Hello, I'm Asare</Text>
+      <Text>Hello, I'm {name}</Text>
       <Text>This Is My First React Native App</Text>
       <Text> Kwame </Text>
-
-      <StatusBar style="auto" />
+      <Button title='woww'/>
+      <StatusBar style="auto"/>
     </View>
   );
 }
