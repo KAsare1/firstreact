1430fbb9c1b1b948dee52a1c7149cb603c222eb2
KAsare1 <asarek977@gmail.com>
2023-01-30 22:47:51 +0000

    text elements
    understanding text elements

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [name, setName] = useState("Kwame");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, I'm Asare</Text>
+      <Text>This Is My First React Native App</Text>
+      <Text> Kwame </Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +22,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent:'center'
+    
+  }
 });
+
